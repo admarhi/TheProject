7bffb50878e4426480334a66f9d3f027e5f4bc81
Adrian Hirt <a.hirt@tuta.io>
2022-05-12 20:18:14 +0200

    working

--- crypto/crypto_selenium.py
+++ crypto/crypto_selenium.py
@@ -12,18 +12,36 @@ ser = Service(gecko_path)
 
 # options = webdriver.chrome.options.Options()
 options = webdriver.firefox.options.Options()
+options.headless = True # set to True to disable browse window
+
 # driver = webdriver.Chrome(options = options, service=ser)
 driver = webdriver.Firefox(options = options, service = ser)
-options.headless = True # set to True to disable browse window
 
 
 url = 'https://coinmarketcap.com/'
 driver.get(url)
 time.sleep(3)
-# table = driver.find_element_by_xpath('//tbody')
-table = driver.find_elements(by = By.XPATH, value = '//a[@class="cmc-link" and re:test(@href, "/currencies/[^/]*/$")]/@href')
-driver.quit()
-print(table)
-for a in table:
-    print(a)
 
+# table = driver.find_element_by_xpath('//a[starts-with(@href, "/currencies/")]')
+# links =[]
+links = driver.find_elements(by = By.XPATH, value = '//a[starts-with(@href, "/currencies/")]')
+driver.quit()
+
+for i in range(len(links)):
+    url = links[i].text
+    driver.get(url)
+    time.sleep(2)
+
+    name = driver.find_element(by = By.XPATH, value = '//h2').text
+    value = driver.find_element(by = By.XPATH, value = '//div[@class="priceValue "]').text
+    change = driver.find_element(by = By.XPATH, value = '//div[@class="priceValue "]/following-sibling::span').text
+
+    stats = driver.find_elements(by = By.XPATH, value ='//div[@class="statsValue"]')
+    marketCap = stats[0].text
+    volumeTraded = stats[4].text
+
+    print(name)
+    print(value)
+    print(change)
+    print(marketCap)
+    print(volumeTraded)
