cb7c6f2e3376a4acabee9adc0556cf26d02dde42
Adrian Hirt <a.hirt@tuta.io>
2022-05-13 15:25:49 +0200

    trying to change user agent

--- crypto/crypto_selenium.py
+++ crypto/crypto_selenium.py
@@ -1,4 +1,5 @@
 
+# from fake_useragent import UserAgent 
 import time
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
@@ -8,9 +9,13 @@ gecko_path = '/usr/local/bin/geckodriver'
 ser = Service(gecko_path)
 options = webdriver.firefox.options.Options()
 options.headless = True # set to True to disable browse window
+# options.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0")
+# ua = UserAgent()
+# userAgent = ua.random
+# options.add_argument(f'user-agent={userAgent}')
 driver = webdriver.Firefox(options = options, service = ser)
 
-url = 'https://coinmarketcap.com/'
+url = 'https://coinmarketcap.com/?page=2'
 driver.get(url)
 time.sleep(3)
 
