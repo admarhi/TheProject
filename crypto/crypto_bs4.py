1eeaf672236bd0ad8c046b108ca8d60932eb853a
Adrian Hirt <a.hirt@tuta.io>
2022-05-13 20:16:19 +0200

    bs4 working

--- crypto/crypto_bs4.py
+++ crypto/crypto_bs4.py
@@ -1,38 +1,31 @@
-from urllib import request
+import urllib
 from bs4 import BeautifulSoup as BS
 import re
+from urllib import request
 
 url = 'https://coinmarketcap.com/'
 html = request.urlopen(url)
-bs = BS(html.read(), 'html.parser')
+bs = BS(html.read(), 'html.parser', from_encoding="iso-8859-1")
 # print(bs)
 
 currencies = []
-list_curr = bs.find("table").find_all('a')
+list_curr = bs.find("table").find_all('a', {'href': re.compile('\/currencies\/[a-z]+/$')})
 currencies = ['https://coinmarketcap.com' + curr['href'] for curr in list_curr]
-print(currencies)
-
+# print(currencies)
 output_data = []
 
-for url in currencies:
-    print(url)
-    html2 = request.urlopen(url)
-    bs2 = BS(html2.read(), "html.parser", from_encoding="iso-8859-1")
-    
-    print(bs2.h2.text) # Name of the currency
-    print(bs2.find('div', {'class':"priceValue"}).text) # Value of the currency (USD)
-    print(bs2.find('div', {'class':"statsValue"}).text) # Market cap
-    print(bs2.find_all('div', {'class':"statsValue"})[4].text) # Volume traded in the last 24 hours
-    print(bs2.find('div', {'class':"priceValue"}).nextSibling.text) # Change today
-    
-    # try:
-    #     name = bs2.h2[2].text
-    #     value = bs2.find('div', {'class':"priceValue"}).text
-    #     marketCap = bs2.find('div', {'class':"statsValue"}).text
-    #     volumeTraded = bs2.find_all('div', {'class':"statsValue"})[4].text
-    #     change = bs2.find('div', {'class':"priceValue"}).nextSibling.text
-    #     output_data.append([name, value, change, marketCap, volumeTraded])
-    # except:
-    #     print(f'Error scraping {currencies[i]}')
+for url2 in currencies:
+    print(url2)
+    html2 = request.urlopen(url2)
+    bs2 = BS(html2.read(), 'html.parser', from_encoding="iso-8859-1")
+    try:
+        name = bs2.h2[2].text # Name of the currency
+        value = bs2.find('div', {'class':"priceValue"}).text # Value of the currency (USD)
+        marketCap = bs2.find('div', {'class':"statsValue"}).text # Market cap
+        volumeTraded = bs2.find_all('div', {'class':"statsValue"})[4].text # Volume traded in the last 24 hours
+        change = bs2.find('div', {'class':"priceValue"}).nextSibling.text # Change today
+        output_data.append([name, value, change, marketCap, volumeTraded])
+    except:
+        print(f'Error scraping {url2}')
 
 print(output_data)
