2168552b154258628aea4958289bef71657277c4
ztcieslinska <66334334+ztcieslinska@users.noreply.github.com>
2022-05-14 08:04:32 +0200

    Limiter and time added
    Still working!

--- selenium/crypto_selenium.py
+++ selenium/crypto_selenium.py
@@ -1,4 +1,3 @@
-
 from fake_useragent import UserAgent 
 import time
 from selenium import webdriver
@@ -7,8 +6,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from csv import writer
 import re
+import time
+
+start_time = time.time()
 
 gecko_path = '/usr/local/bin/geckodriver'
+gecko_path = '/home/nights/Desktop/WEBSCRAPING/geckodriver'
 ser = Service(gecko_path)
 
 options = Options()
@@ -17,14 +20,24 @@ options.headless = True
 # options.add_argument('user-agent=Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)')
 driver = webdriver.Firefox(options = options, service = ser)
 
-url = 'https://coinmarketcap.com/?page=2'
-driver.get(url)
-time.sleep(3)
-# list_curr = driver.find_elements(by = By.XPATH, value = '//a[@class="cmc-link" and re:test(@href, "/currencies/.*/$")]/@href')
-# xpath = '//a[@class="cmc-link" and re:test(@href, "\/currencies/[^/]*/$")]/@href'
-xpath = '//a[starts-with(@href, "/currencies/")]'
-list_curr = driver.find_elements(by = By.XPATH, value = xpath)
-print(list_curr)
+limiter = True
+if limiter:
+    url = 'https://coinmarketcap.com/?page=1'
+    driver.get(url)
+    time.sleep(3)
+    # list_curr = driver.find_elements(by = By.XPATH, value = '//a[@class="cmc-link" and re:test(@href, "/currencies/.*/$")]/@href')
+    # xpath = '//a[@class="cmc-link" and re:test(@href, "\/currencies/[^/]*/$")]/@href'
+    xpath = '//a[starts-with(@href, "/currencies/")]'
+    list_curr = driver.find_elements(by = By.XPATH, value = xpath)
+else:
+    urls = ['https://coinmarketcap.com/?page='+str(i) for i in range(100)]
+    list_curr = []
+    for url in urls:
+        driver.get(url)
+        time.sleep(3)
+        xpath = '//a[starts-with(@href, "/currencies/")]'
+        list_curr.append(driver.find_elements(by = By.XPATH, value = xpath))
+
 links = [link.get_attribute('href') for link in list_curr]
 
 print('#####################################')
@@ -68,3 +81,9 @@ for i in range(len(clean_links)):
     except:
         print(clean_links[i], 'is not working')
 driver.quit()
+
+#print difference between current time at the end and start time
+print("RUNTIME --- %s seconds ---" % (time.time() - start_time))
+
+################# RUNTIME --- 437.41893911361694 seconds --- ##########
+#### without waiting times, estimated: 134 seconds
