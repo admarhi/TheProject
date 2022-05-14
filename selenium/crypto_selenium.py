d062897cf2d7495b9ff2839e9461f3ef2e848d2d
Adrian Hirt <a.hirt@tuta.io>
2022-05-14 21:52:50 +0200

    selenium finalized

--- selenium/crypto_selenium.py
+++ selenium/crypto_selenium.py
@@ -1,4 +1,3 @@
-from fake_useragent import UserAgent 
 import time
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
@@ -11,13 +10,11 @@ import time
 start_time = time.time()
 
 gecko_path = '/usr/local/bin/geckodriver'
-gecko_path = '/home/nights/Desktop/WEBSCRAPING/geckodriver'
-ser = Service(gecko_path)
+# gecko_path = '/home/nights/Desktop/WEBSCRAPING/geckodriver'
 
+ser = Service(gecko_path)
 options = Options()
 options.headless = True
-# userAgent = UserAgent().random
-# options.add_argument('user-agent=Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)')
 driver = webdriver.Firefox(options = options, service = ser)
 
 limiter = True
@@ -25,8 +22,6 @@ if limiter:
     url = 'https://coinmarketcap.com/?page=1'
     driver.get(url)
     time.sleep(3)
-    # list_curr = driver.find_elements(by = By.XPATH, value = '//a[@class="cmc-link" and re:test(@href, "/currencies/.*/$")]/@href')
-    # xpath = '//a[@class="cmc-link" and re:test(@href, "\/currencies/[^/]*/$")]/@href'
     xpath = '//a[starts-with(@href, "/currencies/")]'
     list_curr = driver.find_elements(by = By.XPATH, value = xpath)
 else:
@@ -39,30 +34,13 @@ else:
         list_curr.append(driver.find_elements(by = By.XPATH, value = xpath))
 
 links = [link.get_attribute('href') for link in list_curr]
-
-print('#####################################')
-# for s in list_curr:
-#     print(s.text)
-    # links.append('http://coinmarketcap.com/' + s.text) 
-
-# links = ['https://coinmarketcap.com' + curr['href'] for curr in list_curr]
-
-print('#####################################')
-print(links)
-print('#####################################')
-
 regex = re.compile("https:\/\/coinmarketcap\.com\/currencies\/[a-z]+-*[a-z]+\/$")
 clean_links = [s for s in links if regex.match(s)]
-
-print('#####################################')
-print(clean_links)
-print('#####################################')
-
 
 for i in range(len(clean_links)):
     try:
         url = clean_links[i]
-        print("############## This is the url", url)
+        print("############## This url is being scraped:", url)
         driver.get(url)
         time.sleep(3)
         name = driver.find_element(by = By.XPATH, value = '//h2').text
@@ -73,7 +51,6 @@ for i in range(len(clean_links)):
         volumeTraded = stats[4].text
         
         output_data = [name, value, change, marketCap, volumeTraded]
-        # file = open('selenium_results.csv', 'a', newline='')
         with open('selenium_results.csv', 'a', newline='') as file:
             writer_object = writer(file)
             writer_object.writerow(output_data)
